5c40a1c8e62349ab0f075192d51ad26a3f824606
Kaixinguo360 <kaixinguo360@gmail.com>
2018-04-14 00:30:02 +0800

    Update udp.php

--- udp.php
+++ udp.php
@@ -1,29 +1,33 @@
-<?
+<?php
 error_reporting(E_ALL);
 //set_time_limit(40);
 
 $logFile = "log.txt";
 $fh = fopen($logFile, 'w');
 
-echo "Server Begin...";
-fwrite($fh, "Server Begin...");
+mylog("Server Begin...");
 
 $address = 0;
 $port = 1234;
-
 $socket = stream_socket_server("udp://" . $address . ":" . $port, $errno, $errstr, STREAM_SERVER_BIND);
+
 if (!$socket) {
     die("$errstr ($errno)");
+    fwrite ($fh, "\nEnd at: ".time());
+    fclose ($fh);
 }
 	
 do {
-	$packet = stream_socket_recvfrom($socket, 128, 0, $peer);
-	echo "$peer -- " . $packet . "\n";
-  fwrite($fh, "$peer -- " . $packet . "\n");
-	stream_socket_sendto($socket, $whoami . '/' . date('Y-m-d H:i:s') . '/' . $peer . "\r\n", 0, $peer);
-	echo '> REPLY SENT!' . "\n";
-  fwrite($fh, '> REPLY SENT!' . "\n");
+    $packet = stream_socket_recvfrom($socket, 128, 0, $peer);
+    mylog("Receive Packet:" . $packet);
+    stream_socket_sendto($socket, $whoami . '/' . date('Y-m-d H:i:s') . '/' . $peer . "\r\n", 0, $peer);
 } while ($packet !== false);
+
+function mylog($data) {
+    echo $data;
+    fwrite($fh, $data);
+    return 0;
+}
 
 fwrite ($fh, "\nEnd at: ".time());
 fclose ($fh);
