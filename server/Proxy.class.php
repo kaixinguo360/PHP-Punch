bbc2829ffcc6022ab858061fe88ba050959f4e80
Kaixinguo360 <kaixinguo360@gmail.com>
2018-04-15 02:42:37 +0800

    Update Proxy.class.php

--- server/Proxy.class.php
+++ server/Proxy.class.php
@@ -4,6 +4,8 @@ class Proxy {
     /* 成员变量 */
     var $address, $socket, $name;
     var $status, $lastHB;
+    var $target, $targetPort;
+    var $selfPort;
     
     /* 构造函数 */
     function __construct($socket1, $address1) {
@@ -41,7 +43,7 @@ class Proxy {
             } else {
                 $this -> status = -1;
             }
-        } else if($this -> status == 2) {
+        } else if($this -> status >= 2) {
             if($data != "HB") {
                 $this -> toDo($data);
             }
@@ -49,9 +51,62 @@ class Proxy {
     }
     
     function toDo($data){
-        if($data == "LIST") {
+        global $proxys;
+        if($this -> status >= 3) {
+            $this -> prepare($data);
+        } else if($data == "LIST") {
             $this -> send(getList());
+        } else if(substr($data,0, 3) == "CNT") {
+            $this -> status = 3;
+            $this -> prepare($data);
+            
         }
+    }
+    
+    function prepare($data){
+        if($data == "HB") {
+            return;
+        } else if(substr($data, 0, 3) != "CNT") {
+            $this -> status = 2;
+            $this -> send("NON");
+            return;
+        }
+        $data = substr($data, 3);
+        if($this -> status >= 5) {
+            $this -> p2p($data);
+        } else if($this -> status == 3) {
+            $this -> target = getProxy($data);
+            if($this -> target) {
+                $info = $this -> target -> connect($this);
+                $this -> send("CNTACCEPT:" . $info);
+                $this -> status = 4;
+                $this -> mylog($this -> name . " > > Try CNT " . $data);
+            } else {
+                $this -> status = 2;
+                $this -> send("NON");
+            }
+        } else if($this -> status == 4) {
+            if(substr($data, 0, 4) == "PORT") {
+                $this -> selfPort = substr($data, 4);
+                if($this -> target -> selfPort) {
+                    $this -> status = 5;
+                    $this -> send("CNTOK" . $this -> selfPort);
+                }
+            } else {
+                $this -> status = 2;
+                $this -> send("NON");
+            }
+        }
+    }
+    
+    function p2p($data){
+        
+    }
+    
+    function connect($porxy){
+        $data = $this -> name . ":" . $this -> address;
+        
+        return $data;
     }
     
     function check(){
