b1b3c04c0be04339d41f90164e116235b0e1c824
Kaixinguo360 <kaixinguo360@gmail.com>
2018-04-15 14:44:01 +0800

    Update main.php

--- server/main.php
+++ server/main.php
@@ -1,147 +1,76 @@
 <?php
-class Proxy {
-    
-    /* 成员变量 */
-    var $address, $socket, $name;
-    var $status, $lastHB;
-    var $target;
-    var $selfPort;
-    
-    /* 构造函数 */
-    function __construct($socket1, $address1) {
-        $this -> address = $address1;
-        $this -> name = $address1;
-        $this -> socket = $socket1;
-        $this -> status = 0;
-        $this -> lastHB =time();
-        $this -> mylog("Host " . $this -> address . " Connect...");
-    }
-    
-    /* 成员函数 */
-    function receive($data){
-        $this -> mylog($this -> name . " --> " . $data, 1);
-        $this -> lastHB = time();
-        
-        if($data == "SYN" && $this -> status != 0) {
-            $this -> status = 0;
-        } else if($this -> status == -1) {
-            $this -> send("RETRY");
-        } else if($this -> status == 0) {
-            if($data == "SYN") {
-                $this -> status = 1;
-                $this -> send("ACK");
-            } else {
-                $this -> status = -1;
-            }
-        } else if($this -> status == 1) {
-            $str = substr($data,0, 3);
-            if($str == "ACK") {
-                $this -> name = substr($data,3);
-                $this -> status = 2;
-                $this -> send("ACCEPT");
-                $this -> mylog("Host " . $this -> address . " >>> " . $this -> name);
-                //$this -> mylog($this -> name . " >>> Login! (" . $this -> address . ")");
-            } else {
-                $this -> status = -1;
-            }
-        } else if($this -> status >= 2) {
-            if($data != "HB") {
-                $this -> toDo($data);
-            }
+
+
+/** Init **/
+
+error_reporting(E_ALL);
+define("LOG_LEVEL", 1);
+require_once 'Proxy.class.php';
+require_once 'MyLoger.class.php';
+
+$myloger = new MyLoger("log.txt");
+$socket = getSocket(0, 1234);
+$proxys;
+
+
+/** Main Loop **/
+
+do {
+    $packet = stream_socket_recvfrom($socket, 128, 0, $peer);
+    check();
+    if(!$proxys[$peer])
+        $proxys[$peer] = new Proxy($socket, $peer);
+    $proxys[$peer] -> receive($packet);
+} while (true);
+
+
+/** Functions **/
+
+function getProxy($name) {
+    global $proxys;
+    foreach ($proxys as $address => $proxy) {
+        if($proxy -> name == $name) {
+            return $proxy;
         }
     }
-    
-    function toDo($data){
-        global $proxys;
-        if($this -> status >= 3) {
-            $this -> prepare($data);
-        } else if($data == "LIST") {
-            $this -> send("LIST" . getList());
-        } else if(substr($data,0, 3) == "CNT") {
-            $this -> status = 3;
-            $this -> prepare($data);
+    return NULL;
+}
+
+function getList() {
+    global $proxys;
+    $list;
+    foreach ($proxys as $address => $proxy) {
+        if($proxy -> status >= 2) {
+            $list = $list . $proxy -> name . "\n";
         }
     }
-    
-    function prepare($data){
-        if($data == "HB") {
-            return;
-        } else if(substr($data, 0, 3) != "CNT") {
-            $this -> disConnect();
-            return;
-        } else if(substr($data, 0, 3) == "NON") {
-            $this -> disConnect();
-            return;
-        }
-        $data = substr($data, 3);
-        if($this -> status >= 5) {
-            $this -> p2p($data);
-        } else if($this -> status == 3) {
-            $target = getProxy($data);
-            if($target && ($target -> target == NULL || $target -> target == $this)) {
-                $info = $target -> touch($this);
-                $this -> target = $target;
-                $this -> mylog($this -> name . " > > " . $data . " Try...");
-                if($target -> target == $this) {
-                    $this -> send("CNTACCEPT:" . $info);
-                    $this -> status = 4;
-                    $this -> mylog($this -> name . " >*> " . $data . " Accpet...");
-                }
-            } else {
-                $this -> disConnect();
-            }
-        } else if($this -> status == 4) {
-            if(substr($data, 0, 4) == "PORT") {
-                $this -> selfPort = substr($data, 4);
-                if($this -> target -> selfPort) {
-                    $this -> status = 5;
-                    $this -> send("CNTOK" . $this -> target -> selfPort);
-                    $this -> mylog($this -> name . " >-> " . $this -> target -> name . " Prepared!");
-                }
-            } else {
-                $this -> disConnect();
-            }
+    return $list;
+}
+
+function check() {
+    global $proxys;
+    foreach ($proxys as $address => $proxy) {
+        if(!$proxy -> check()) {
+            mylog($proxys[$address] -> name . " -X- Lost !");
+            unset($proxys[$address]);
         }
     }
+}
+
+function getSocket($address, $port) {
+	
+    $socket = stream_socket_server("udp://" . $address . ":" . $port, $errno, $errstr, STREAM_SERVER_BIND);
     
-    function p2p($data){
-        
+    if (!$socket) {
+        die("$errstr ($errno)");
     }
     
-    function touch($porxy){
-        if($this -> status == 2) {
-            $this -> send("REQ" . $porxy -> name);
-        }
-        
-        $data = $this -> name . ":" . $this -> address;
-        return $data;
-    }
+    mylog("Server Begin...");
     
-    function disConnect(){
-        $this -> status = 2;
-        $this -> send("NON");
-        $this -> target = NULL;
-        $this -> selfPort = NULL;
-    }
-    
-    function check(){
-        $time = time() - $this -> lastHB;
-        
-        if($time < 15) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    
-    function send($data){
-        $this -> mylog($this -> name . " <-- " . $data, 1);
-        stream_socket_sendto($this -> socket, $data, 0, $this -> address);
-    }
-    
-    function mylog($data, $p = 0){
-        if($p < LOG_LEVEL) {
-            mylog($data);
-        }
-    }
+    return $socket;
+}
+
+function mylog($data) {
+    global $myloger;
+    $myloger -> log($data);
 }
