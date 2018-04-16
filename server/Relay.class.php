885455982bca9210c27df3942fc48b96ca0935d9
Kaixinguo360 <kaixinguo360@gmail.com>
2018-04-16 17:37:02 +0800

    Add Relay.class
    Add Relay.class to help connecting when p2p is unavailable.

---
+++ server/Relay.class.php
@@ -0,0 +1,51 @@
+<?php
+class Relay{
+    
+    /* 成员变量 */
+    var $peer1;
+    var $peer2;
+    var $socket;
+    var $lastHB;
+    
+    /* 构造函数 */
+    function __construct($peer1, $peer2, $socket) {
+        $this -> peer1 = $peer1;
+        $this -> peer2 = $peer2;
+        
+        $this -> socket = $socket;
+        
+        $this -> mylog("Create New Relay " . $peer1 . " <==> " . $peer2);
+    }
+    
+    /* 成员函数 */
+    function receive($peer, $data){
+        $this -> lastHB = time();
+        mylog("===>" . $data);
+        if($peer == $this -> peer1) {
+            $this -> send($this -> peer2, $data);
+        } else if($peer == $this -> peer2) {
+            $this -> send($this -> peer1, $data);
+        }
+    }
+    
+    function send($peer, $data){
+        $this -> mylog($this -> name . " <=== " . $data, 1);
+        stream_socket_sendto($this -> socket, $data, 0, $peer);
+    }
+    
+    function isAlive(){
+        $time = time() - $this -> lastHB;
+        
+        if($time < TTL_RELAY) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    function mylog($data, $p = 0){
+        if($p < LOG_LEVEL) {
+            mylog($data);
+        }
+    }
+}
